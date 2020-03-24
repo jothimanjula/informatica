02115873b567f11c3be785c47fe9cc698c68c97d
Manjula Jothilingam <41056909+jothimanjula@users.noreply.github.com>
2020-03-24 20:52:41 +0530

    Add files via upload

---
+++ informatics/src/app/models/Useraccounts.ts
@@ -0,0 +1,17 @@
+import { Time } from '@angular/common';
+
+export class Useraccounts
+{
+    public UserId : number;
+    public FirstName : string;
+    public LastName : string;
+    public UserName : string;
+    public Password : string;
+    public Active : boolean
+    public CreatedDate : Date
+    public CreatedBy : string;
+    public ModifiedDate : Date
+    public ModifedBy : string;
+    public DeletedDate : Date
+    public DeletedBy : string;
+}
